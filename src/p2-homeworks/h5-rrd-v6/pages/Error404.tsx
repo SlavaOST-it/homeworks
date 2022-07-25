665197847a7778640ac73f73bd346ae359c42809
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-07-25 21:13:25 +0300

    Add Error page for HW5

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import s from "./Error.module.css"
 
 function Error404() {
+
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.error_page}>
+            <div className={s.error_404}>Ooops... Error 404</div>
+            <div className={s.error_text}>Sorry, but the page you are looking for doesn't exist.</div>
+            <div>
+                <img className={s.error_img} src={'https://cdn.wallpapersafari.com/84/92/kiyMfq.jpg'} alt={"error-img"}/>
+            </div>
         </div>
     )
 }
