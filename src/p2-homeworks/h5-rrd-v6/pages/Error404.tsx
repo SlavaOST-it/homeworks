063999377efeca2564a68a102812a959b5bb7f61
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-07-26 14:42:22 +0300

    add button 'back'

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import s from "./Error.module.css"
+import {useNavigate} from "react-router-dom";
 
 function Error404() {
+
+    const navigate = useNavigate()
 
     return (
         <div className={s.error_page}>
             <div className={s.error_404}>Ooops... Error 404</div>
             <div className={s.error_text}>Sorry, but the page you are looking for doesn't exist.</div>
+            <button className={s.error_btn} onClick={()=>{navigate(-1)}}>BACK</button>
             <div>
                 <img className={s.error_img} src={'https://cdn.wallpapersafari.com/84/92/kiyMfq.jpg'} alt={"error-img"}/>
             </div>
