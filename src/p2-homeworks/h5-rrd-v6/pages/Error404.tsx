c695d9a61957c4368dd1302071dcf13d20cd1883
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-07-26 14:47:49 +0300

    change button to universal <ButtonMy>

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from "./Error.module.css"
 import {useNavigate} from "react-router-dom";
+import ButtonMy from "../../h4/common/c2-SuperButton/ButtonMy";
 
 function Error404() {
 
@@ -10,9 +11,11 @@ function Error404() {
         <div className={s.error_page}>
             <div className={s.error_404}>Ooops... Error 404</div>
             <div className={s.error_text}>Sorry, but the page you are looking for doesn't exist.</div>
-            <button className={s.error_btn} onClick={()=>{navigate(-1)}}>BACK</button>
+            <ButtonMy title={'BACK'}
+                      callBack={() => {navigate(-1)}} disable={false} className={s.error_btn}/>
             <div>
-                <img className={s.error_img} src={'https://cdn.wallpapersafari.com/84/92/kiyMfq.jpg'} alt={"error-img"}/>
+                <img className={s.error_img} src={'https://cdn.wallpapersafari.com/84/92/kiyMfq.jpg'}
+                     alt={"error-img"}/>
             </div>
         </div>
     )
